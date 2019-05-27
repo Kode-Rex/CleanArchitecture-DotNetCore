edb828678c7e3c758e533e86b4018a535db4b598
Treveshan Naidoo <treveshan.naidoo@gmail.com>
2019-05-27 20:50:36 +0200

    feat: add CsvFileOutput

---
+++ source/StoneAge.CleanArchitecture.Domain/Messages/CsvFileOutput.cs
@@ -0,0 +1,11 @@
+﻿namespace StoneAge.CleanArchitecture.Domain.Messages
+{
+    public class CsvFileOutput : InMemoryFileOutput
+    {
+        private const string _contentType = "text/csv";
+
+        public CsvFileOutput(string fileName, byte[] fileData) : base(fileName, fileData, _contentType)
+        {
+        }
+    }
+}
